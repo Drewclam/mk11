d5efe3a002554751f79c83505a0a9cfe4a26320a
Drewclam <andrew.lam@rangle.io>
2019-04-25 16:29:01 -0400

    Setup server with dummy endpoint

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/', (req, res) => res.send({ message: 'Hello world' }));
+
+app.listen(port, () => console.log(`Backend listening on port ${port}`));
